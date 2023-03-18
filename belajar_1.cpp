2aa6153e3786827e2f35e252d77a3a3d86045e10
Ganang Aji <124345005+GanangAji05@users.noreply.github.com>
2023-03-18 18:12:40 +0700

    FIX TOTAL
    This coding simple from branch main,This coding fix

--- belajar_1.cpp
+++ belajar_1.cpp
@@ -3,95 +3,86 @@
 #include <iostream>
 #include <cmath>
 #include <string>
+#include <conio.h>
 
 using namespace std;
 
+
 int main() {
+    long b,d ;
+    string a,c,x;
+    float z ,y,Bobot,total = 0,total2 = 0,IPK;
     cout << "Selamat Datang "<<endl;
-    string x = "Masukkan Nama Mahasiswa : ";
-    cout << x ;
-    cin >> x ;
-    int y ; 
-    cout << "Masukkan NIM Mahasiswa : ";
-    cin >> y;
-    string z= "Masukkan Kelas Mahasiswa : ";
-    cout << z ;
-    cin >> z;
-
+    cout << "Masukkan Nama Mahasiswa  : ";  cin >> a;
+    cout << "Masukkan NIM Mahasiswa   : ";  cin >> b;
+    cout << "Masukkan Kelas Mahasiswa : ";  cin >> c;
+    cout << "Masukkan Jumlah Matkul   : ";  cin >> d;
     {
-    string x;  
-    int y , z;
-    float a ;
-    cout << "Masukkan Nama Matkul : ";
-    cin >> x;
-    cout << "Masukkan Jumlah SKS : ";
-    cin >> y ;
-    cout << "Masukkan Jumlah Nilai : ";
-    cin >> z ;
+    for (int i= 0; i < d ; i++){
+        cout << endl;
+        cout << "Masukkan Nama Matkul  : ";   cin >> x;
+        cout << "Masukkan Jumlah SKS   : ";   cin >> y ;
+        cout << "Masukkan Jumlah Nilai : ";   cin >> z ;
+
+            if (z >= 86 && z <= 100) {
+                Bobot = y * 4.00 ;
+                cout << "Bobot NIlai : " << Bobot ; 
+                cout  <<endl;
+                cout << "Huruf : A"<<endl;}
+                else if (z >= 81 && z <=86 ){
+                    Bobot = y * 3.67;
+                    cout << "Bobot NIlai : " << Bobot ;  
+                    cout <<endl;
+                    cout << "Huruf : A-"<<endl;
+                }else if (z >= 76 && z <=80 ){
+                    Bobot = y * 3.33;
+                    cout << "Bobot NIlai : " << Bobot ;
+                    cout <<endl;  
+                    cout << "Huruf : B+"<<endl;
+                }else if (z >= 71 && z <=75 ){
+                    Bobot = y * 3.00;
+                    cout << "Bobot NIlai : " << Bobot ;  
+                    cout <<endl;
+                    cout << "Huruf : B"<<endl;
+                }else if (z >= 66 && z <=70 ){
+                    Bobot = y * 2.67;
+                    cout << "Bobot NIlai : " << Bobot ;  
+                    cout <<endl;
+                    cout << "Huruf : B-"<<endl;
+                }else if (z >= 61 && z <=65 ){
+                    Bobot = y * 2.33;
+                    cout << "Bobot NIlai : " << Bobot ;  
+                    cout <<endl;
+                    cout << "Huruf : C+"<<endl;
+                }else if (z >= 56 && z <=60 ){
+                    Bobot = y * 2.00;
+                    cout << "Bobot NIlai : " << Bobot ;  
+                    cout <<endl;
+                    cout << "Huruf : C"<<endl;
+                }else if (z >= 41 && z <=55 ){
+                    Bobot = y * 1.00;
+                    cout << "Bobot NIlai : " << Bobot ;  
+                    cout <<endl;
+                    cout << "Huruf : D"<<endl;
+                }else if (z >= 0 && z <= 40 ){
+                    Bobot = y * 0,00;
+                    cout << "Bobot NIlai : " << Bobot ;  
+                    cout <<endl;
+                    cout << "Huruf : E"<<endl;
+                }else{
+                    cout << "Nilai yang Anda Masukkan Salah, Sikahkan masukkan ulang" ;
+                }
+                total = total + Bobot;
+                cout << "Total Bobot : " << total << endl ;
+                total2 = total2 + y;
+                cout << "Total SKS   : " << total2 << endl;
+                }}
+        cout << endl ;
+        IPK = total / total2;
+        cout << "Nilai IPK Anda : " << IPK;
+        cin.get();
+        return 0;   
+ }
     
-    // Percabangan Nilai
-        if (z >= 86 ) {
-            cout << "Bobot Nilai : " ;
-            a = y * 4.00;
-            cout << a <<endl;
-            cout << "Huruf : A"<<endl;}
-            else if (z >= 81 && z <=86 ){
-                cout << "Bobot Nilai : " ;
-                a = y * 3.67;
-                cout << a <<endl;
-                cout << "Huruf : A-"<<endl;
-            }else if (z >= 76 && z <=80 ){
-                cout << "Bobot Nilai : " ;
-                a = y * 3.33;
-                cout << a <<endl;
-                cout << "Huruf : B+"<<endl;
-            }else if (z >= 71 && z <=75 ){
-                cout << "Bobot Nilai : " ;
-                a = y * 3.33;
-                cout << a <<endl;
-                cout << "Huruf : B"<<endl;
-            }
 
-
-            {
-                string x; 
-                int y1 , z1;
-                float b;
-                cout << "Masukkan Nama Matkul : ";
-                cin >> x;
-                cout << "Masukkan Jumlah SKS : ";
-                cin >> y1 ;
-                cout << "Masukkan Jumlah Nilai : ";
-                cin >> z1 ;
-            
-                if (z1 >= 86 ) {
-                    cout << "Bobot Nilai : " ;
-                    b = y1 * 4.00;
-                    cout << b <<endl;
-                    cout << "Huruf : A"<<endl;
-                    } else if (z1 >= 81 && z1 <=86 ){
-                        cout << "Bobot Nilai : " ;
-                        b = y1 * 3.67;
-                        cout << b <<endl;
-                        cout << "Huruf : A-"<<endl;   
-                    } else if (z1 >= 76 && z1 <=80 ){
-                        cout << "Bobot Nilai : " ;
-                        b = y1 * 3.33;
-                        cout << b <<endl;
-                        cout << "Huruf : B+"<<endl;
-                    }else if (z1 >= 71 && z1 <=75 ){
-                        cout << "Bobot Nilai : " ;
-                        b = y1 * 3.33;
-                        cout << b <<endl;
-                        cout << "Huruf : B"<<endl;}
-        {
-            float hasil = a + b;
-            float IPK = hasil / 2;
-            cout << IPK;
-        }
-                
-        }
-    }
-    return 0;
-}
 
